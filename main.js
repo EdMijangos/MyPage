ce2d8a8f75e6295ee1d21b6bf20ec3fb31df4d59
EdMijangos <mloeza2010@hotmail.com>
2018-08-05 10:22:34 -0500

    fixes for mobile displays

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ $(window).scroll( function(){ //scroll listener
   /* Check the location of each desired element */
   $('.hideme').each( function(i){
     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-    var bottom_of_window = $(window).scrollTop() + $(window).height();
+    var bottom_of_window = $(window).scrollTop() + $(window).height() + 60;
 
     /* If the object is completely visible in the window, fade it in */
     if( bottom_of_window > bottom_of_object ){
